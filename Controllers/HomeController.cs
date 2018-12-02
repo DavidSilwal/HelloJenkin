83a0c6a5bb3b4cc7a3b08d712c73ea95d609012f
David Silwal <de.davidsilwal@gmail.com>
2018-12-02 20:23:28 +0545

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace HelloJenkins.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Hello Auzre DevOps Pipeline.";
 
             return View();
         }
